09c46de01198fd3f5682a956bc53896eb2f4a8f0
viliyam vasava <viliyam2811@gmail.com>
2024-02-14 19:05:20 +0530

    change

---
+++ search.js
@@ -0,0 +1,14 @@
+
+const posts = [ 
+{
+  id: "clsd19v530001ggs68hp4c1xo",
+  title: "How to Install and Setup Php Mysql in Termux?",
+  permalink: "https://termhub.xyz/post/how-to-install-and-setup-php-mysql-in-termux/"
+},
+
+{
+  id: "clsd19v610003ggs69jrjhct7",
+  title: "Unlock Your Phone's Secret Superpower: The Beginer's Termux Guide.",
+  permalink: "https://termhub.xyz/post/unlock-your-phones-secret-superpower-the-beginer-termux-guide/"
+},
+];
