30bf8f9fa63ae5778cec32733ce1d490e34b186b
viliyam vasava <viliyam2811@gmail.com>
2024-02-15 20:25:14 +0530

    save

--- js/search.js
+++ js/search.js
@@ -3,12 +3,12 @@ const posts = [
 {
   id: "clsnbbol60001xas6bg0zd57g",
   title: "How to Install and Setup Php Mysql in Termux?",
-  permalink: "/post/how-to-install-and-setup-php-mysql-in-termux/"
+  permalink: "/xion198/post/how-to-install-and-setup-php-mysql-in-termux/"
 },
 
 {
   id: "clsnbbomy000kxas6bw8gdtcc",
   title: "Unlock Your Phone's Secret Superpower: The Beginer's Termux Guide.",
-  permalink: "/post/unlock-your-phones-secret-superpower-the-beginer-termux-guide/"
+  permalink: "/xion198/post/unlock-your-phones-secret-superpower-the-beginer-termux-guide/"
 },
 ];
